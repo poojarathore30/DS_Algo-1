be8f7a1ee603921a75a2d25ede4a828aa5fe1a9c
suraj0223 <surajvijay71@gmail.com>
2020-09-15 00:09:09 +0530

    basic file structure added to algo4.cpp

--- algo4.cpp
+++ algo4.cpp
@@ -1,6 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main() {
-    // write your code here 
+
+    /*
+    for making the file as a standard input and standard output use 
+    freopen("input.txt","r", stdin);
+    freopen("output.txt", "w", stdout);
+    */
+
+    // start your logic here
+    
     return 0;
 }
